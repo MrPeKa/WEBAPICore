6be93e69379d0e71554d54f97b4f5f6ca4f234c2
MrPeKa <pekunicki@hotmail.com>
2017-01-14 21:55:16 +0100

    Changed assemblies and adjusted name of libraries

--- src/CoreWEBAPI.Database/Properties/AssemblyInfo.cs
+++ src/CoreWEBAPI.Database/Properties/AssemblyInfo.cs
@@ -5,15 +5,18 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CoreWEBAPI.Database")]
+[assembly: AssemblyProduct("CoreWEBAPI.EntityFramework")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2e5b77d6-94aa-42b3-92f5-9c0a701cb516")]
+
+[assembly: Guid("2a85f21d-5d01-4537-b0ef-cda5b83996cf")]
